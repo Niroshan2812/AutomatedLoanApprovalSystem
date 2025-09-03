375d26e03cae8b5d5177b8e8fc728eebc2ab5d92
Niroshan <niroshan9812@gmail.com>
2025-09-04 02:31:29 +0530

    issue01

---
+++ loan-approval-system/src/main/java/com/loanapproval/loan_approval_system/LoanApprovalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.loanapproval.loan_approval_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApprovalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApprovalSystemApplication.class, args);
+	}
+
+}
---
+++ loan-approval-system/src/test/java/com/loanapproval/loan_approval_system/LoanApprovalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanapproval.loan_approval_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApprovalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
