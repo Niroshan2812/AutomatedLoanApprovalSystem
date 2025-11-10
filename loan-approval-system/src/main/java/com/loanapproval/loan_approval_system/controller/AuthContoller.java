96d8ea98394ef90e199c1d850eaeba58830f9c26
Niroshan <niroshan9812@gmail.com>
2025-11-10 16:39:30 +0530

    Sample req

--- loan-approval-system/src/main/java/com/loanapproval/loan_approval_system/controller/AuthContoller.java
+++ loan-approval-system/src/main/java/com/loanapproval/loan_approval_system/controller/AuthContoller.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 //fifth test
 //sixth test
 //seventh test
+//eight test
 @RestController
 @RequestMapping("api/auth")
 public class AuthContoller {
